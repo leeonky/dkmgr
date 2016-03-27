dddaafddf19d98ebbfb38a724a3e9b686cc7ed29
leeonky <leeonky@gmail.com>
2016-03-27 14:34:47 +0000

    start and stop passed

--- dkmgr.sh
+++ dkmgr.sh
@@ -10,15 +10,41 @@ docker_run() {
 	sudo docker run --name $2 -d --privileged=true -v ~/share:/home/devuser/share/:rw -v ~/.ssh:/home/devuser/.ssh $1
 }
 
+is_container_running() {
+	[ "$(sudo docker ps --filter "name=$1" -q)" != "" ]
+}
+
+is_container_created() {
+	[ "$(sudo docker ps --filter "name=$1" -qa)" != "" ]
+}
+
 start_container() {
 	local inc_name=$2
 	local image_name=$1
-	local tag=$(image_tag_of $image_name)
-	docker_run $image_name:$tag $inc_name
+	if ! is_container_running $inc_name; then
+		if ! is_container_created $inc_name; then
+			local tag=$(image_tag_of $image_name)
+			docker_run $image_name:$tag $inc_name
+		else
+			instance_run $inc_name
+		fi
+	fi
+}
+
+stop_container() {
+	sudo docker stop $1
 }
 
 docker_management() {
 	local inc_name=$3
 	local image_name=$2
-	start_container $image_name $inc_name
+	case $1 in
+	start)
+		start_container $image_name $inc_name
+	;;
+	stop)
+		stop_container $inc_name
+	;;
+	esac
 }
+
--- dkmgr_test.sh
+++ dkmgr_test.sh
@@ -7,7 +7,9 @@ test_container_first_start() {
 	mock_verify start_container HAS_CALLED_WITH 'img' 'inc'
 }
 
-test_start_container() {
+test_start_container_with_dock_run_when_no_contain() {
+	mock_function is_container_running 'return 1'
+	mock_function is_container_created 'return 1'
 	mock_function image_tag_of 'echo 1'
 	mock_function docker_run
 
@@ -41,8 +43,83 @@ test_docker_run() {
 	docker_run 'img:0' 'inc'
 
 	mock_verify sudo HAS_CALLED_WITH docker run --name inc -d --privileged=true -v ~/share:/home/devuser/share/:rw -v ~/.ssh:/home/devuser/.ssh 'img:0'
-
 }
 
+test_start_container_again() {
+	mock_function is_container_running
+	mock_function docker_run
+
+	start_container 'img' 'inc'
+
+	mock_verify is_container_running HAS_CALLED_WITH 'inc'
+	mock_verify docker_run NEVER_CALLED
+}
+
+test_is_container_running_when_stopped() {
+	mock_function sudo
+
+	is_container_running 'inc'
+	
+	assertFalse $?
+	mock_verify sudo HAS_CALLED_WITH docker ps --filter 'name=inc' -q
+}
+
+test_is_container_running_when_running() {
+	mock_function sudo 'echo aaa'
+
+	is_container_running 'inc'
+	
+	assertTrue $?
+	mock_verify sudo HAS_CALLED_WITH docker ps --filter 'name=inc' -q
+}
+
+test_restart_container() {
+	mock_function is_container_running 'return 1'
+	mock_function is_container_created
+	mock_function image_tag_of
+	mock_function docker_run
+	mock_function instance_run
+
+	start_container 'img' 'inc'
+
+	mock_verify is_container_created HAS_CALLED_WITH 'inc'
+	mock_verify instance_run HAS_CALLED_WITH 'inc'
+	mock_verify image_tag_of NEVER_CALLED
+	mock_verify docker_run NEVER_CALLED
+}
+
+test_is_container_created_when_created() {
+	mock_function sudo 'echo id'
+
+	is_container_created 'inc'
+
+	assertTrue $?
+	mock_verify sudo HAS_CALLED_WITH docker ps --filter 'name=inc' -qa
+}
+
+test_is_container_created_when_not_created() {
+	mock_function sudo
+
+	is_container_created 'inc'
+
+	assertFalse $?
+	mock_verify sudo HAS_CALLED_WITH docker ps --filter 'name=inc' -qa
+}
+
+test_stop_runner() {
+	mock_function stop_container
+
+	docker_management stop img inc
+
+	mock_verify stop_container HAS_CALLED_WITH 'inc'
+}
+
+test_stop_container() {
+	mock_function sudo
+
+	stop_container inc
+
+	mock_verify sudo HAS_CALLED_WITH docker stop 'inc'
+}
 
 . $SHUNIT2_BIN
